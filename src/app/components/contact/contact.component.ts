b73422df52d30633b38d2386f3f601942489a3d8
Cleiton Oliveira <cleiton.o.dev@gmail.com>
2025-03-02 19:19:50 +0000

    Add dark mode support to Contact component

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.scss'],
   standalone: true
 })
-export class ContactComponent {}
+export class ContactComponent {
+  @Input() isDarkMode = false;
+}
