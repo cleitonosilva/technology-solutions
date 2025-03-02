1f61a4c17072f1bfd943ff0d68d1974d5f42e777
Cleiton Oliveira <cleiton.o.dev@gmail.com>
2025-03-02 12:44:55 +0000

    Add header component with responsive design and dark mode support

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { DesktopMenuComponent } from './desktop-menu/desktop-menu.component';
+import { MobileMenuComponent } from './mobile-menu/mobile-menu.component';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+    DesktopMenuComponent,
+    MobileMenuComponent
+  ]
+})
+export class HeaderComponent {
+  @Input() isDarkMode = false;
+  @Output() darkModeToggle = new EventEmitter<void>();
+
+  onToggleDarkMode(): void {
+    this.darkModeToggle.emit();
+  }
+}
